0f5b0c5b91b687ab4e1a7179914647a241cd93be
JuanMPaola <juanmpaola25@gmail.com>
2023-10-23 15:53:38 -0300

    Probando B)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { UsersModule } from './users/users.module';
 import { ImmovablesModule } from './immovables/immovables.module';
 import { SharedModule } from './shared/shared.module';
 
+//Es una prueba
+
 @Module({
   imports: [UsersModule, ImmovablesModule, SharedModule],
   controllers: [AppController],
